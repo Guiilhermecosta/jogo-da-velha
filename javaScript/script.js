b32b0df4389777e1365d991b7e0925406556f7f2
Guilheme C. Costa <gguillhherrmmecosta@gmail.com>
2025-06-01 22:20:08 -0300

    código otimizado
    reduzir um pouco o código, principalmente HTML, projeto refeito praticamente,  com  algumas melhorias

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,124 +1,17 @@
-/*function tema()}
-var corpo = window.document.body
-corpo.style.background = "black"
-}*/
-var x1 = document.querySelector("div.x1");
+ const container = document.getElementById("container");
+    let currentPlayer = "X";
 
-function one() {
-  console.log("clicado");
-  x1.style.background = "blue";
-  x1.innerHTML = "X";
-}
-function ones() {
-  x1.innerHTML = "";
-  x1.style.background = "orange";
-  x1.innerHTML = "O";
-  console.log("clique duplo");
-}
-/*function foco() {
-  x1.style.border = "2px solid green";
-}*/
-var x2 = document.querySelector("div.x2");
+    // Criação das células dinamicamente
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("click", () => handleClick(cell));
+      container.appendChild(cell);
+    }
 
-function two() {
-  console.log("clicado");
-  x2.style.background = "blue";
-  x2.innerHTML = "X";
-}
-function two2() {
-  x2.innerHTML = "";
-  console.log("clique duplo");
-  x2.style.background = "orange";
-  x2.innerHTML = "O";
-}
-var x3 = document.querySelector("div.x3");
-
-function three() {
-  console.log("clicado");
-  x3.style.background = "blue";
-  x3.innerHTML = "X";
-}
-function three3() {
-  x3.innerHTML = "";
-  console.log("clique duplo");
-  x3.style.background = "orange";
-  x3.innerHTML = "O";
-}
-var x4 = document.querySelector("div.x4");
-
-function four() {
-  console.log("clicado");
-  x4.style.background = "blue";
-  x4.innerHTML = "X";
-}
-function four4() {
-  x4.innerHTML = "";
-  console.log("clique duplo");
-  x4.style.background = "orange";
-  x4.innerHTML = "O";
-}
-var x5 = document.querySelector("div.x5");
-
-function five() {
-  console.log("clicado");
-  x5.style.background = "blue";
-  x5.innerHTML = "X";
-}
-function five5() {
-  x5.innerHTML = "";
-  console.log("clique duplo");
-  x5.style.background = "orange";
-  x5.innerHTML = "O";
-}
-var x6 = document.querySelector("div.x6");
-
-function six() {
-  console.log("clicado");
-  x6.style.background = "blue";
-  x6.innerHTML = "X";
-}
-function six6() {
-  x6.innerHTML = "";
-  console.log("clique duplo");
-  x6.style.background = "orange";
-  x6.innerHTML = "O";
-}
-var x7 = document.querySelector("div.x7");
-
-function seven() {
-  console.log("clicado");
-  x7.style.background = "blue";
-  x7.innerHTML = "X";
-}
-function seven7() {
-  x7.innerHTML = "";
-  console.log("clique duplo");
-  x7.style.background = "orange";
-  x7.innerHTML = "O";
-}
-var x8 = document.querySelector("div.x8");
-
-function eight() {
-  console.log("clicado");
-  x8.style.background = "blue";
-  x8.innerHTML = "X";
-}
-function eight8() {
-  x8.innerHTML = "";
-  console.log("clique duplo");
-  x8.style.background = "orange";
-  x8.innerHTML = "O";
-}
-var x9 = document.querySelector("div.x9");
-
-function nine() {
-  console.log("clicado");
-  x9.style.background = "blue";
-  x9.innerHTML = "X";
-}
-function nine9() {
-  x9.innerHTML = "";
-  console.log("clique duplo");
-  x9.style.background = "orange";
-  x9.innerHTML = "O";
-}
+    function handleClick(cell) {
+      cell.textContent = currentPlayer;
+      cell.classList.add("taken");
+      // adições futuras no projeto
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
